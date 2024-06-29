a120706387d885c0d3651e15b143300c8faa7943
Masoumeh Avarideh <m.avarideh@gmail.com>
2024-06-29 12:01:18 +0330

    Session 1-COM

---
+++ Session 1- center of mass.py
@@ -0,0 +1,21 @@
+#m1=10,r1=10,m2=20,r2=10
+
+#section 1
+m1=int(input("m1:"))
+r1=int(input("r1:"))
+m2=int(input("m2:"))
+r2=int(input("r2:"))
+
+#section 2
+q1=(m1*r1)
+q2=(m2*r2)
+
+s1=q1+q2
+
+#section
+
+s2=m1+m2
+
+#section 4
+COM=s1/s2
+print(f"com:{COM}")
