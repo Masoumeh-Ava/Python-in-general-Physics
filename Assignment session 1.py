4149cfcb65e904bde499fd70f7aff3a2512cc7d3
Masoumeh Avarideh <m.avarideh@gmail.com>
2024-06-29 11:58:44 +0330

    Assiegment s1
    Python  in Physics

---
+++ Assignment session 1.py
@@ -0,0 +1,13 @@
+import math as m
+g=9.8
+m=eval(input("enter Mass(kg): "))
+v0=eval(input("enter velocity (m/s):"))
+teta=eval(input("enter angle (degree):"))
+#d=eval(input("enter the destance of inclined surface (m):"))
+h=((v0**2)/(2*g))
+#5d=(h*(m.sin(teta)))
+print(f"hight is{h}")
+
+
+
+
